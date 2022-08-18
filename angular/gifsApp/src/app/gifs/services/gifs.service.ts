12951267fc59d3fc6fffcb6d3f23c1fd43e75b43
Eduardo Flores <efloresm@minsait.com>
2022-08-18 23:48:41 +0200

    Updated buscarGifs method and sidebar componet with titlecase pipe

--- angular/gifsApp/src/app/gifs/services/gifs.service.ts
+++ angular/gifsApp/src/app/gifs/services/gifs.service.ts
@@ -7,14 +7,34 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'amRPLANlckz856YW2Bkm1dA9D7isTh5g';
   private _historial: string[] = [];
 
   get historial(){
+    
+
     return [...this._historial] // se rompe la referencia con el operador spread y regresa un nuevo arreglo
   }
 
-  buscarGifs(query:string){
+  buscarGifs(query:string= '' ){
+    // convertir a minuscula todas las palabras
+    query = query.trim().toLocaleLowerCase();
+    // limitar la cantidad de inserciones que podemos tener en el historial
+    this._historial = this._historial.splice(0,10);
+
+    // si existe
+    const found = this._historial.find(item=> item===query);
     // agrego elemento al principio del arreglo
-    this._historial.unshift(query);
+    // si es distinto a la palabra de busqueda entonces la agrego a la lista
+    if(found!==query) {
+      this._historial.unshift(query);
+    }
+
+    // solucion 2
+    // si no esta en la lista la palabra de busqueda
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+   } 
+
   }
 }
