20660a73d56b469e0f11bdffddc7a4f03879f0a9
Eduardo Flores <efloresm@minsait.com>
2022-08-30 17:17:20 +0200

    Created pais table component with an argument paises as Input parameter from por-pais component, updated pais.module references, and export pais tabla as well.

---
+++ angular/paisesApp/src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+  styleUrls: ['./pais-tabla.component.css']
+})
+export class PaisTablaComponent {
+  
+  // paises viene desde por-pais component
+  @Input() paises: Country [] = [];
+  
+  constructor() { }
+
+
+}
